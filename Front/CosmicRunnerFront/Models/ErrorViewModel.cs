f614bb13753eb049d6042a6203365341dfd32c13
Victor de la Rosa <delarosasanovic@gmail.com>
2026-04-20 14:05:18 -0600

    Initialized Frontend Project

---
+++ Front/CosmicRunnerFront/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CosmicRunnerFront.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
