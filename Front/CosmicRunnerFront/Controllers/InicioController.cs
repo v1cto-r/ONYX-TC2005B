0c427e179e008bd81bcabe7ae7fb5ae3344ea775
GioMax102 <A01572090@tec.mx>
2026-04-27 20:35:31 -0600

    Cuestionario Agregar Idea

--- Front/CosmicRunnerFront/Controllers/InicioController.cs
+++ Front/CosmicRunnerFront/Controllers/InicioController.cs
@@ -1,6 +1,8 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using CosmicRunnerFront.Models;
+using System.ComponentModel.Design;
+using CosmicRunnerFront.Models.InicioModels;
 
 namespace CosmicRunnerFront.Controllers;
 
@@ -9,7 +11,21 @@ public class InicioController : Controller
 
     public IActionResult Index()
     {
+        ViewData["NuevaIdea"] = new CrearIdeaViewModel();
         return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult CrearIdea(CrearIdeaViewModel modelo)
+    {
+        if (ModelState.IsValid)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+        TempData["ReabrirModalNuevaIdea"] = true;
+        ViewData["NuevaIdea"] = modelo;
+        return View(Index);
     }
 
     public IActionResult Privacy()
