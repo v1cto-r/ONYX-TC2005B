a9790c67356fbb2e435ba0dc4b28d1f18e9516dc
Marino Emilio Cáceres Suárez <marinoe2005@gmail.com>
2026-05-23 15:35:57 -0600

    !feat: Implementacion de pagina web a desarrollar UsuarioPrompts

---
+++ Front/CosmicRunnerFront/Controllers/UsuarioController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using CosmicRunnerFront.Models;
+
+namespace CosmicRunnerFront.Controllers;
+
+public class UsuarioController : Controller
+{
+    private readonly ILogger<UsuarioController> _logger;
+
+    public UsuarioController(ILogger<UsuarioController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
