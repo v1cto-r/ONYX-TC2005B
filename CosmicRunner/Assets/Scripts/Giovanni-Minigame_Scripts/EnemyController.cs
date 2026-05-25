e596be78fa95399513168ecf358aed3de43b0430
GioMax102 <A01572090@tec.mx>
2026-05-25 12:03:08 -0600

    Acomodo de velocidades

--- CosmicRunner/Assets/Scripts/Giovanni-Minigame_Scripts/EnemyController.cs
+++ CosmicRunner/Assets/Scripts/Giovanni-Minigame_Scripts/EnemyController.cs
@@ -10,43 +10,50 @@ public class EnemyShip : MonoBehaviour
     public int daño = 2; 
 
     [Header("Impacto (Knockback)")]
-    [Tooltip("Fuerza con la que el enemigo empuja al jugador al chocar")]
     public float fuerzaEmpuje = 15f;
 
     private Rigidbody2D rb;
+    
+    // VARIABLES PARA MANTENER LA BARRIDA ESTABLE
+    private Transform camaraTransform;
+    private float offsetY;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.gravityScale = 0f;
         Destroy(gameObject, tiempoDeVida);
+
+        // 1. Guardamos la referencia de la cámara principal
+        camaraTransform = Camera.main.transform;
+        
+        // 2. Calculamos a qué distancia vertical (Y) nació el enemigo respecto a la cámara
+        offsetY = transform.position.y - camaraTransform.position.y;
     }
 
     void FixedUpdate()
     {
-        // Como rotamos el prefab en Z, su nariz ahora es "transform.up"
-        // Si el enemigo viaja de derecha a izquierda, asegúrate de que la rotación Z lo haga apuntar bien.
+        // 1. Aplicamos la velocidad horizontal normal
         rb.linearVelocity = transform.up * velocidadEmbiste; 
+
+        // 2. TRUCO DE PANTALLA: Forzamos la posición Y para que viaje "pegado" a la cámara
+        Vector2 posicionCorregida = rb.position;
+        posicionCorregida.y = camaraTransform.position.y + offsetY;
+        rb.position = posicionCorregida;
     }
 
-    // Usamos OnTriggerEnter2D porque ahora la nave es un Trigger (Atraviesa)
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            // 1. Buscamos el Rigidbody del jugador para empujarlo
             Rigidbody2D rbJugador = collision.GetComponent<Rigidbody2D>();
             
             if (rbJugador != null)
             {
-                // 2. Calculamos la dirección del empuje (Desde el enemigo hacia el jugador)
                 Vector2 direccionEmpuje = (collision.transform.position - transform.position).normalized;
-                
-                // 3. Aplicamos un impulso violento en esa dirección
                 rbJugador.AddForce(direccionEmpuje * fuerzaEmpuje, ForceMode2D.Impulse);
             }
 
-            // Aquí llamarías al GameManager para restar vidas.
             Debug.Log("Jugador golpeado. -2 Vidas. Aplicando Knockback.");
         }
     }
