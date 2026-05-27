a65becb6d1326ccac22555e119cdc12dbe83cf34
Jorge Vazquez <A00842238@tec.mx>
2026-05-26 23:05:55 -0600

    Agregacion de Sumar Monedas en Minijuego

--- CosmicRunner/Assets/Scripts/Jorge-Minigame_Scripts/UI/EndGameManager.cs
+++ CosmicRunner/Assets/Scripts/Jorge-Minigame_Scripts/UI/EndGameManager.cs
@@ -4,47 +4,59 @@ using TMPro;
 
 namespace JorgeGame
 {
-public class EndGameManager : MonoBehaviour
-{
-    // define si es victoria o derrota
-    public bool isVictory;
-
-    // referencias al panel y texto de monedas
-    public GameObject coinsPanel;
-    public TextMeshProUGUI coinsText;
-
-    void Start()
+    public class EndGameManager : MonoBehaviour
     {
-        SFXManager.instance.musicSource.Stop();
-        
-        if (isVictory)
+        // Define victoria o derrota
+        public bool isVictory;
+
+        // UI final
+        public GameObject coinsPanel;
+        public TextMeshProUGUI coinsText;
+
+        // Conexion API
+        public JorgeCreditsApi jorgeCreditsApi;
+
+        void Start()
         {
-            SFXManager.instance.PlaySFX(SFXManager.instance.winSound, 0.2f);
-            // obtiene las monedas guardadas y las muestra
-            int coins = GameControl.Instance.coins;
-            coinsText.text = "+" + coins;
-            coinsPanel.SetActive(true);
+            SFXManager.instance.musicSource.Stop();
+
+            if (isVictory)
+            {
+                SFXManager.instance.PlaySFX(SFXManager.instance.winSound, 0.2f);
+
+                int coins = GameControl.Instance.coins;
+
+                coinsText.text = "+" + coins;
+                coinsPanel.SetActive(true);
+
+                if (jorgeCreditsApi == null)
+                {
+                    jorgeCreditsApi = FindAnyObjectByType<JorgeCreditsApi>();
+                }
+
+                if (jorgeCreditsApi != null)
+                {
+                    jorgeCreditsApi.SendCredits(coins);
+                }
+            }
+            else
+            {
+                SFXManager.instance.PlaySFX(SFXManager.instance.loseSound, 0.2f);
+
+                coinsPanel.SetActive(false);
+            }
         }
-        else
+
+        public void RestartGame()
         {
-            SFXManager.instance.PlaySFX(SFXManager.instance.loseSound, 0.2f);
-            // en derrota no se muestran monedas
-            coinsPanel.SetActive(false);
+            Time.timeScale = 1f;
+            SceneManager.LoadScene("GameScene_Jorge");
+        }
+
+        public void GoToMenu()
+        {
+            Time.timeScale = 1f;
+            SceneManager.LoadScene("MainMenuScene_Jorge");
         }
     }
-
-    public void RestartGame()
-    {
-        // reinicia el nivel actual
-        Time.timeScale = 1f;
-        SceneManager.LoadScene("GameScene_Jorge");
-    }
-
-    public void GoToMenu()
-    {
-        // regresa al menu principal
-        Time.timeScale = 1f;
-        SceneManager.LoadScene("MainMenuScene_Jorge");
-    }
-}
 }
